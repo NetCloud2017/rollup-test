b5efe6ef002f0890c1448a67b67ee6123aa2a4c8
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2022-10-09 18:46:46 +0800

    rollup 插件使用。

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 console.log("hellw rollup   ");
 export const x = 12;
+import React from "react";
+import pkg from "./package.json";
+console.log(pkg.version);
+React.useEffect(function func() {});
--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,37 @@
 console.log(process.env.TEST, process.env.MODE);
+// 第三方库合并到本地代码
+import resolve from "@rollup/plugin-node-resolve";
+// 让rollup 支持 commonjs 文件
+import commonjs from "@rollup/plugin-commonjs";
+// rollup  处理JSON 文件
+import json from "@rollup/plugin-json";
+const plugins = [resolve(), commonjs(), json()];
+
+import { terser } from "rollup-plugin-terser";
+// 输出的第三方使用的 插件很少， 一般是压缩， 等。
+const outputPlugins = [terser()];
+
 export default [
   {
     input: "./main.js",
+    // 不打包到一起。
+    external: ["react"],
+    plugins,
     output: {
       file: "dist/index.umd.js",
       format: "umd",
       name: "Index",
+      plugins: outputPlugins,
     },
   },
   {
     input: "./main.js",
+    external: ["react"],
+    plugins,
     output: {
       format: "es",
       file: "dist/index.es.js",
+      plugins: outputPlugins,
     },
   },
 ];
