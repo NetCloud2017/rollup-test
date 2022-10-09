4ee66b293b00de765a0aec0230062902aec4c2e5
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2022-10-09 18:16:32 +0800

    打包成不同的模块输出文件

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,18 @@
-console.log(process.env.TEST);
-export default {
-  input: "./main.js",
-  output: {
-    file: "dist.js",
-    format: "umd",
-    name: "Index",
+console.log(process.env.TEST, process.env.MODE);
+export default [
+  {
+    input: "./main.js",
+    output: {
+      file: "dist/index.umd.js",
+      format: "umd",
+      name: "Index",
+    },
   },
-};
+  {
+    input: "./main.js",
+    output: {
+      format: "es",
+      file: "dist/index.es.js",
+    },
+  },
+];
